2810ea739b557d21b0bbe55ee302e66ec39ac338
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-07-19 12:37:59 -0400

    built in list methods started

--- mutable.py
+++ mutable.py
@@ -13,6 +13,7 @@ print(id(another_list))     # 140610540762816
 
 shopping_list += ["cookies"]
 print(shopping_list)    # ['milk', 'pasta', 'eggs', 'bacon', 'bread', 'rice', 'cookies']
+<<<<<<< HEAD
 print(another_list)    # ['milk', 'pasta', 'eggs', 'bacon', 'bread', 'rice', 'cookies']
 print(id(another_list))     # 140610540762816
 
@@ -25,3 +26,6 @@ print(c)    # ['milk', 'pasta', 'eggs', 'bacon', 'bread', 'rice', 'cookies', 'cr
 print(f)    # ['milk', 'pasta', 'eggs', 'bacon', 'bread', 'rice', 'cookies', 'cream']
 print(shopping_list)    # ['milk', 'pasta', 'eggs', 'bacon', 'bread', 'rice', 'cookies', 'cream']
 
+=======
+print(id(another_list))     # 140610540762816
+>>>>>>> 92bd27dc76033166094381e4bcad8c46396fe257
