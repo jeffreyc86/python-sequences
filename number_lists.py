9b80f1877d607bba0cedc8ade17d64e647992208
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-07-19 12:36:00 -0400

    built in list methods started

---
+++ number_lists.py
@@ -0,0 +1,19 @@
+even = [2, 4, 6 ,8]
+odd = [1, 3, 5, 7, 9]
+
+print(min(even))    # 2
+print(max(even))    # 8
+
+print(min(odd))     # 1
+print(max(odd))     # 9
+
+print()     # line break
+
+print(len(even))    # 4
+print(len(odd))     # 5
+
+print()
+
+print("mississippi".count("s"))     # 4
+print("mississippi".count("iss"))     # 2
+
